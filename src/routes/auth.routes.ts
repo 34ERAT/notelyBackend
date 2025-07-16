fc10bac5ec04bdd90cc20f0015d021c4ea94b50d
34ERAT <philipkanuti@gmail.com>
2025-07-16 22:33:17 +0300

    redo completed todos

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
 import { login, logout, register } from "../controllers";
-import router from ".";
 const authRouter = Router();
-//TODO: POST /api/auth/register: register a user
 authRouter.post("/register", register);
-//TODO: POST /api/auth/login: login a user.
 authRouter.post("/login", login);
-//TODO: POST /api/auth/logout: logout a user.
 authRouter.post("/logout", logout);
 //TODO: POST /api/auth/password: update a specific user's password.
 // router.post("/password");
