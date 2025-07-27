e8417d88e80662b89a1361ebfa145f3292469f72
34ERAT <philipkanuti@gmail.com>
2025-07-27 17:36:55 +0300

    change origing to origin

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   port: Number(process.env.PORT) || 3000,
   jwtSecret: process.env.JWT_SECRET as string,
   jwtSecretRefresh: process.env.JWT_REFRESH_SECRET as string,
-  origin: process.env.ORIGING as string,
+  origin: process.env.ORIGIN as string,
   cloud_name: process.env.CLOUD_NAME as string,
   api_key: process.env.COULDINARY_API_KEY as string,
   api_secret: process.env.COULDINARY_API_SECRET as string,
